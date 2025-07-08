9b13a5080543cb8e295dd0994fbd083c2afde8b9
JazelleTamayo <jazellet5@gmail.com>
2025-07-08 19:48:28 +0800

    Add files via upload

---
+++ Chapter1/ExtractingData.py
@@ -0,0 +1,11 @@
+students = [
+    ["BSIT",["David", "Alener"]],
+    ["BSCS",["Jaymar", "Emman", "Patrick"]]
+]
+
+for i in students:
+    print(i[0])
+    for x in i[1]:
+        print(x)
+    print()
+
