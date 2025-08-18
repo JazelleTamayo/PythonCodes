8fb7fcfc8929d6aa5ffeb946bffc7af6bce518aa
JazelleTamayo <jazellet5@gmail.com>
2025-08-18 09:14:20 +0800

    Add files via upload

---
+++ Chapter 2/app1.py
@@ -0,0 +1,15 @@
+"""
+	this is block comment
+	---------------------
+	A program that would display 'Hello World'
+"""
+#this is a line comment
+#import the necessary library to clear the console screen
+
+import os
+
+#call the module(function) to clear console screen
+
+os.system('cls')
+
+print('Hello world')
